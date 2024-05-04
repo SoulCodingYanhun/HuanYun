5ff03a7a395cec9b0b107ae93d113eea6edd6656
SoulCodingYanhun <155857563+SoulCodingYanhun@users.noreply.github.com>
2024-05-04 15:47:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <header>
+      <v-app-bar :elevation="0">
+        <v-app-bar-title class="text-gradient"><span class="text-gradient">幻云科技</span></v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-heart"></v-btn>
+
+          <v-btn icon="mdi-magnify"></v-btn>
+
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
 </template>
 
-<script></script>
+<script>
+</script>
+
+<style>
+.text-gradient {
+  background: linear-gradient(to right, rgb(235 125 247), rgb(0, 213, 255));
+  -webkit-background-clip: text;
+  color: transparent;
+  /* font-size: 2em; */
+}
+</style>
